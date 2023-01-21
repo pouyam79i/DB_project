bba75c82d49a776a11511759913b1307ac717672
Pouya <pouyamohammadyirbu@gmail.com>
2023-01-21 22:29:36 +0330

    inserting 10 customers + some small debugging

---
+++ P3/phase-3.sql
@@ -0,0 +1,111 @@
+/* Adding customers */
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "1004003434",
+    "09211117799",
+    "Ali",
+    "Ramazani",
+    "Tehran",
+    "Tehran",
+    "Meidan vali asr - sakhteman erfan vahed 3"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "2004343534",
+    "09211117383",
+    "Reza",
+    "Khardali",
+    "Khorasan Razavi",
+    "Mashhad",
+    "Meidan razavi - sakhteman emam vahed 3"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "2004313534",
+    "09234317388",
+    "Mohhamd",
+    "Haghjo",
+    "Khorasan Razavi",
+    "Neishabor",
+    "Meidan Jahad - pelack 8"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "1004873454",
+    "09214117799",
+    "Maede",
+    "Razipor",
+    "Tehran",
+    "Tehran",
+    "Tehran pars -Meidan X - sakhteman azadeh vahed 2"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "3494033431",
+    "09171131729",
+    "Pouya",
+    "Mohammadi",
+    "Bushehr",
+    "Bushehr",
+    "Meidan emam - pelak 3"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "3491332439",
+    "09171117732",
+    "Ali",
+    "Jafari",
+    "Bushehr",
+    "Bushehr",
+    "Meidan emam - pelak 3"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "5493452439",
+    "09131217732",
+    "Zahra",
+    "Jafari",
+    "Esfehan",
+    "Esfehan",
+    "Meidan nesfe jahan - pelak 3"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "5493452121",
+    "09131517331",
+    "Mahan",
+    "Rahmani",
+    "Esfehan",
+    "Esfehan",
+    "Meidan nesfe jahan - pelak 2"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "1011032434",
+    "09233314499",
+    "Ahmad",
+    "Ahmadvand",
+    "Tehran",
+    "Tehran",
+    "Meidan enqelab - sakhteman sina vahed 5"
+);
+
+INSERT INTO Customers (NN, PHONE, F_NAME, L_NAME, ADDRS_PROV, ADDRS_CITY, ADDRS_DETAIL)
+VALUES (
+    "1044123434",
+    "09212317799",
+    "Kaveh",
+    "Koshan",
+    "Tehran",
+    "Tehran",
+    "Meidan vali asr - sakhteman nejat vahed 2"
+);
+
